6e2a589a95786383ffd24a775a4db475c4a259d5
Jesse <Existance@jesses-MacBook-Pro.local>
2025-01-29 10:51:45 -0500

    added JS and adjusted html

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const getPosts = async() =>{
-  const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-  const allPosts = await response.json();
-  console.log(allPosts);
+const state = {
+  posts: []
 }
 
+const getPosts = async () => {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts`); //gets data
+  const allPosts = await response.json();// gets data
+  state.posts = allPosts;  // stores data 
+  renderallPosts();
+}
+
+const renderallPosts = () => {
+  const ol = document.createElement(`ol`); //creates ol
+  state.posts.forEach((singlePost) => {      // goes through each post
+    const li = document.createElement(`li`); // creates li
+    li.innerText = singlePost.title;  // putting title of the post in the li
+    ol.append(li); // append the li to the ol
+
+  })
+  //console.log(ol); // showing the li in the ol
+  const main = document.querySelector(`main`);  // grabs the main
+  main.append(ol); // appends the ol in the main
+}
 
 getPosts();
-
 
