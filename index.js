1c45df844074447b345da81d909475c86f870ee9
Jesse <Existance@jesses-MacBook-Pro.local>
2025-01-29 11:13:03 -0500

    display post details

--- index.js
+++ index.js
@@ -14,6 +14,24 @@ const renderallPosts = () => {
   state.posts.forEach((singlePost) => {      // goes through each post
     const li = document.createElement(`li`); // creates li
     li.innerText = singlePost.title;  // putting title of the post in the li
+
+
+    //add event listener for click to the li
+    
+    li.addEventListener(`click`, () =>{
+      const detailsHTML = `
+      <h2>${singlePost.title}</h2>
+      <p>${singlePost.body}</p>
+      `;
+      main.innerHTML = detailsHTML;
+
+    });
+      // get the details for the post that was clicked
+      //replace the main element with the details
+
+
+
+
     ol.append(li); // append the li to the ol
 
   })
